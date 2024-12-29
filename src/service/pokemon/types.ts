cc83da87d15d0f179c0564f6c1dd69fba915de33
orlandomunoz <or.munozf@gmail.com>
2024-12-29 20:42:06 -0300

    change: modify class types and add evolution class

--- src/service/pokemon/types.ts
+++ src/service/pokemon/types.ts
@@ -1,47 +1,61 @@
 export class PokemonList {
-    results: { 
-        name: string; 
-        url: string }[];
+  results: {
+    name: string;
+    url: string
+  }[];
 
-    constructor(data: any) {
-        this.results = data.results || [];
-    }
+  constructor(data: any) {
+    this.results = data.results || [];
+  }
 }
 
 export class ShowPokemon {
-    id: number;
-    name: string;
-    image: string;
-    types: string[];
-    height: number;
-    weight: number;
-    stats: { name: string; value: number }[];
-    sound: string;
-    description: string;
-    evolutionChain: { name: string; image: string; trigger: string | null; min_level: number | null }[]
-  
-    constructor(
-      id: number,
-      name: string,
-      image: string,
-      types: string[],
-      height: number,
-      weight: number,
-      stats: { name: string; value: number }[],
-      sound: string,
-      description: string,
-      evolutionChain: any
-    ) {
-      this.id = id;
-      this.name = name;
-      this.image = image;
-      this.types = types;
-      this.height = height;
-      this.weight = weight;
-      this.stats = stats;
-      this.sound = sound;
-      this.description = description;
-      this.evolutionChain = evolutionChain;
-    }
+  id: number;
+  name: string;
+  image: string;
+  types: string[];
+  height: number;
+  weight: number;
+  stats: { name: string; value: number }[];
+  sound: string;
+  description: string;
+  evolutionChain: { name: string; image: string; trigger: string | null; min_level: number | null }[]
+
+  constructor(
+    id: number,
+    name: string,
+    image: string,
+    types: string[],
+    height: number,
+    weight: number,
+    stats: { name: string; value: number }[],
+    sound: string,
+    description: string,
+    evolutionChain: any
+  ) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.types = types;
+    this.height = height;
+    this.weight = weight;
+    this.stats = stats;
+    this.sound = sound;
+    this.description = description;
+    this.evolutionChain = evolutionChain;
   }
-  
+}
+
+
+export class Evolution {
+  name: string;
+  image: string;
+
+  constructor(
+    name: string,
+    image: string,
+  ) {
+    this.name = name;
+    this.image = image;
+  }
+}
