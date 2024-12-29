5c0999e9a294240cd227e193a1644112e21ed66b
orlandomunoz <or.munozf@gmail.com>
2024-12-29 17:01:46 -0300

    change: delete import.meta.env.VITE_BASE_URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,8 +20,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router
