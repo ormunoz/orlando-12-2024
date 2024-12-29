6cb600bbe286e7761e17fb72fedd7e3d8e36cb90
orlandomunoz <or.munozf@gmail.com>
2024-12-29 00:05:07 -0300

    feat: url api configuration

---
+++ src/configs/UrlConfig.ts
@@ -0,0 +1,26 @@
+export enum EnvNames {
+  PROD = 'production',
+  DEV = 'development',
+}
+type EnvName = EnvNames.PROD | EnvNames.DEV;
+
+export interface EnvConfig {
+  apiURL: string;
+  env: EnvName;
+}
+
+const ENV_CONFIGS: Record<EnvName, EnvConfig> = {
+  production: {
+    apiURL: process.env.VITE_API_URL || 'https://pokeapi.co/api/v2/',
+    env: EnvNames.PROD,
+  },
+  development: {
+    apiURL: process.env.VITE_API_URL || 'https://pokeapi.co/api/v2/',
+    env: EnvNames.DEV,
+  },
+};
+
+export function getEnvConfig(): EnvConfig {
+  const nodeEnv = (import.meta.env.MODE as EnvName) || EnvNames.DEV;
+  return ENV_CONFIGS[nodeEnv];
+}
