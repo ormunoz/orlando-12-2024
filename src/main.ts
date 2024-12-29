f429c385fcdc0eac5e9d11e59aca35d4d0887d14
orlandomunoz <or.munozf@gmail.com>
2024-12-29 16:59:40 -0300

    feat: add tailwind configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './index.css'
+import { createPinia } from 'pinia';
+import Vue3Toastify from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
+const pinia = createPinia();
 const app = createApp(App);
+app.use(pinia);
 app.use(router);
+app.use(Vue3Toastify);
 app.mount('#app');
